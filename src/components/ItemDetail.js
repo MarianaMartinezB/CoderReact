0b3a7f95fd90248a9ed9afea1d80a2ac8e33ca16
Mariana Martinez Brizuela <113954180+MarianaMartinezB@users.noreply.github.com>
2023-04-21 21:56:05 -0300

    primer commit

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,7 +1,10 @@
-import Imagenes from './Imagenes'
+import { Link } from 'react-router-dom'
+import { useContext, useState } from "react"
+import { CartContext } from "../context/CartContext"
+import ItemCount from './ItemCount'
 
 
-const ItemDetail = ({ name, price, stock, id}) => {
+/* const ItemDetail = ({ id, name, price, img, stock, description}) => {
     return (
         <div>
             <h2>{name}</h2>
@@ -11,4 +14,42 @@ const ItemDetail = ({ name, price, stock, id}) => {
         </div>
     )
 }
+export default ItemDetail */
+
+const ItemDetail = ({ id, name, price, img, stock, description}) => {
+    const [quantity, setQuantity] = useState (0)
+
+    const { addItem } = useContext(CartContext)
+
+    const handleOnAdd = (quantity) => {
+        const serviceToAdd = {
+            id, name, price, quantity
+        }
+        addItem(serviceToAdd)
+
+        setQuantity(quantity)
+
+    }
+        return (
+            <div style={{background: 'pink', margin: 10}}>
+                <h2>{name}</h2>
+                <img src={img} alt={name} style={{ width: 500}}/>
+                <h3>Price: {price}</h3>
+                <p>Description: {description}</p>
+                <p>Stock: {stock}</p>
+                <footer>
+                    {
+                        quantity === 0 ? (
+                        <ItemCount onAdd={handleOnAdd} stock={stock}/>
+                        ) : (
+                            <Link to='/cart'>Check Out</Link>
+                        )
+                    }
+                </footer>
+                
+
+            </div>
+        )
+    }
+
 export default ItemDetail
---
+++ src/services/firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcUMbgH1QDlmGgWn8haglXwMdSvmQzr7w",
+  authDomain: "backend51150-74d70.firebaseapp.com",
+  projectId: "backend51150-74d70",
+  storageBucket: "backend51150-74d70.appspot.com",
+  messagingSenderId: "375531019041",
+  appId: "1:375531019041:web:0a6377137ad140e04e3c60"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db= getFirestore (app);
+
